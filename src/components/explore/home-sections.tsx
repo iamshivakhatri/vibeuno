2cd7264e92f0aa9b03a69f39ecf67f7843e7e1dc
Shiva Khatri <shiva.khatri0001@gmail.com>
2025-01-25 15:20:31 -0500

    .

--- src/components/explore/home-sections.tsx
+++ src/components/explore/home-sections.tsx
@@ -1,17 +1,17 @@
-"use client";
+// "use client";
 
-import { PeopleSection } from "./people-section";
-import { PlacesSection } from "./places-section";
-import { CitiesSection } from "./cities-section";
-// import { PopularSection } from "./popular-section";
-// import { TrendingSection } from "./trending-section";
+// import { PeopleSection } from "./people-section";
+// import { PlacesSection } from "./places-section";
+// import { CitiesSection } from "./cities-section";
+// // import { PopularSection } from "./popular-section";
+// // import { TrendingSection } from "./trending-section";
 
-export function HomeSections() {
-  return (
-    <div className="space-y-12">
-      <PeopleSection />
-      <PlacesSection />
-      <CitiesSection />
-    </div>
-  );
-}
+// export function HomeSections() {
+//   return (
+//     <div className="space-y-12">
+//       <PeopleSection />
+//       <PlacesSection />
+//       <CitiesSection />
+//     </div>
+//   );
+// }
